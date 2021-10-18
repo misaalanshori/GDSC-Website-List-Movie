7fc6a753f9a07ef338319a3801fd000e4b39b9b7
M Isa Al Anshori <38964181+misaalanshori@users.noreply.github.com>
2021-10-18 12:36:40 +0700

    Created modal dummy and added more data to json

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 var movieData;
 
+
+
+async function getJSON() {
+    const response = await fetch("movies.json");
+    const movieJSON = await response.json()
+    return movieJSON
+}
+
 async function renderList(json) {
-    var itemCopy = document.getElementsByClassName("movieItem")[0].cloneNode(true);
+    let itemCopy = document.getElementsByClassName("movieItem")[0].cloneNode(true);
     itemCopy.getElementsByClassName("loading")[0].classList.remove("loading")
-    var tempHTML = "";
+    let tempHTML = "";
     json.forEach(movie => {
         itemCopy.getElementsByClassName("movieRank")[0].innerHTML = movie.rank
         itemCopy.getElementsByClassName("movieTitle")[0].innerHTML = movie.title
@@ -13,14 +21,34 @@ async function renderList(json) {
     document.getElementById("movieList").innerHTML = tempHTML;
 }
 
-async function getJSON() {
-    const response = await fetch("movies.json");
-    const movieJSON = await response.json()
-    return movieJSON
+function openModal(context, modal) {
+    // Update Modal Here
+    modal.style.display = "block"
 }
+
+
+
+
+
+
+
+
+var movieModal = document.getElementById("movieModal");
+var closeButton = document.getElementsByClassName("closeModal")[0];
 
 getJSON().then(retval => {
     movieData = retval
     renderList(retval)
 })
 
+// When the user clicks on (x), close the modal
+closeButton.onclick = function () {
+    movieModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == movieModal) {
+        movieModal.style.display = "none";
+    }
+}
